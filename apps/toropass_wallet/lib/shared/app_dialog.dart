2e9702747696516247483b5a3adbf772c64935f9
Farouk Bello <faroukk.bello@gmail.com>
2026-06-03 15:02:14 +0100

    refactor: improve dialog display function and update AppTextfield to handle focus management

--- apps/toropass_wallet/lib/shared/app_dialog.dart
+++ apps/toropass_wallet/lib/shared/app_dialog.dart
@@ -11,39 +11,42 @@ Future displayDialog(
   Color barrierColor = const Color(0x80000000),
   required Widget child,
   Function(bool, Object?)? onPopInvokedWithResult,
-}) async => await showGeneralDialog(
-  context: context,
-  barrierDismissible: dismissible,
-  barrierLabel: barrierLabel,
-  barrierColor: barrierColor,
-  pageBuilder: (context, animation, secondaryAnimation) {
-    final appColors = AppColors.of(context);
-    final borderRadius = BorderRadius.circular(AppDimens.dialogBorderRadius);
+}) async {
+  FocusManager.instance.primaryFocus?.unfocus();
+  await showGeneralDialog(
+    context: context,
+    barrierDismissible: dismissible,
+    barrierLabel: barrierLabel,
+    barrierColor: barrierColor,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      final appColors = AppColors.of(context);
+      final borderRadius = BorderRadius.circular(AppDimens.dialogBorderRadius);
 
-    return PopScope(
-      canPop: dismissible && onPopInvokedWithResult == null,
-      onPopInvokedWithResult: onPopInvokedWithResult,
-      child: Center(
-        child: Material(
-          borderRadius: borderRadius,
-          child: Container(
-            width: width,
-            decoration: BoxDecoration(
-              color: appColors.white,
-              borderRadius: borderRadius,
-              boxShadow: [
-                BoxShadow(
-                  color: appColors.shadow.withAlpha(20),
-                  blurRadius: 20,
-                  spreadRadius: 5,
-                  offset: const Offset(0, 10),
-                ),
-              ],
+      return PopScope(
+        canPop: dismissible && onPopInvokedWithResult == null,
+        onPopInvokedWithResult: onPopInvokedWithResult,
+        child: Center(
+          child: Material(
+            borderRadius: borderRadius,
+            child: Container(
+              width: width,
+              decoration: BoxDecoration(
+                color: appColors.white,
+                borderRadius: borderRadius,
+                boxShadow: [
+                  BoxShadow(
+                    color: appColors.shadow.withAlpha(20),
+                    blurRadius: 20,
+                    spreadRadius: 5,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+              ),
+              child: child,
             ),
-            child: child,
           ),
         ),
-      ),
-    );
-  },
-);
+      );
+    },
+  );
+}
